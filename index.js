75eed7eca3a8165592dc6fede7e14cd9e8161b7f
bryan-cda <albuquerque.bry.n@gmail.com>
2025-01-05 11:02:37 -0300

    feat: introduce a primitive data types.

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ let lastName = "Bar";
 
 console.log(firstName);
 console.log(lastName);
+
+const bornDate = 1990;
+console.log(bornDate);
+
+let isOnStore = true;
+console.log(`is available: ${isOnStore}`);
+
+let item = "Drone XJY"
+let itemPrice = 50.00;
+console.log(`The product ${item} have a price ${itemPrice}`);
+console.log(typeof isOnStore);
