bf11351b08122254c0122799f6a8f33236289333
bryan-cda <albuquerque.bry.n@gmail.com>
2025-01-05 11:33:44 -0300

    feat: introduce use of objects.

--- index.js
+++ index.js
@@ -14,5 +14,24 @@ console.log(`is available: ${isOnStore}`);
 
 let item = "Drone XJY"
 let itemPrice = 50.00;
+let releaseDate = 2024;
+let timeFlying = 15;
+let haveAutoReturn = true;
+let haveGPS = true;
+let color = 'yellow';
+
 console.log(`The product ${item} have a price ${itemPrice}`);
 console.log(typeof isOnStore);
+
+let drone = { item, itemPrice, releaseDate, timeFlying, haveAutoReturn, haveGPS, color };
+console.log(drone);
+
+drone.color = 'blue';
+console.log(drone);
+
+drone['itemPrice'] = '$ 55.00';
+console.log(drone);
+
+let friends = ['Foo','Bar', 'John', 'Due'];
+console.log(friends);
+console.log(friends[2]);
