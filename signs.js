788866359fdb4b04b83342f31fcf9b5a8ba8e018
bryan-cda <albuquerque.bry.n@gmail.com>
2025-01-14 20:51:22 -0300

    feat: remove element of an array and clear an array.

--- signs.js
+++ signs.js
@@ -19,10 +19,30 @@ const fireSigns = [
     {id: 3, sign: 'Sagittarius'}
 ];
 
+//When is an array with objects include return false
 console.log(fireSigns.includes({id: 1, sign: 'Aries'}));
 
-
+//When we use find with a function the object inside array is found
 console.log(fireSigns.find(function(fire) {
     return fire.sign == 'Aries';
 }));
    
+//Using arrow function
+console.log(fireSigns.find(fire => fire.sign == 'Aries'));
+
+//Remove the last element in an array
+let lastItem = starSigns.pop();
+console.log(lastItem);
+
+//Remove the first element of an array
+let firstItem = starSigns.shift();
+console.log(firstItem);
+
+//Use splice to remove one or more itens placing by index 
+console.log(`Get out of here: `, starSigns.splice(5, 1));
+console.log(starSigns);
+
+//Clear an array with length or splice
+starSigns.length = 0;
+starSigns.splice(0, starSigns.length);
+console.log(starSigns);
